7df5b90df8e1eef20371319789f762703eb810ca
Wlafi <tkenesbaev12@gmail.com>
2025-03-09 13:11:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ async function registerUser() {
     const username = "testUser";
     const password = "123456";
 
-    const response = await fetch("https://wai-1.onrender.com/api/register", {
+    const response = await fetch("https://wai-1.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
